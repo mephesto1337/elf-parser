d81e93f213fc4f1250832be249166445a15e9a2e
Thomas WACHE <thomas@wache.fr>
2018-04-30 14:49:48 +0200

    Added C API

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@ extern crate enum_primitive;
 #[macro_use]
 extern crate nom;
 
+extern crate libc;
+
 
 #[allow(dead_code)]
 #[allow(unused_macros)]
@@ -13,6 +15,9 @@ pub use header::*;
 
 pub mod elf64;
 pub use elf64::*;
+
+pub mod c_api;
+pub use c_api::*;
 
 #[cfg(test)]
 mod tests {

c5ad9a60da61e3c9e0e5d91c6a2c69885cbc14a8
Thomas WACHE <thomas@wache.fr>
2018-05-01 15:57:17 +0200

    Added Exe and Section trait implementation

--- src/elf64/mod.rs
+++ src/elf64/mod.rs
@@ -1,4 +1,5 @@
 use nom;
+use exe;
 
 pub mod types64;
 pub use types64::*;
@@ -23,4 +24,42 @@ pub fn parse_elf64(i: &[u8]) -> nom::IResult<&[u8], Elf64> {
         segments:   segments.1,
         sections:   sections.1
     }))
+}
+
+impl exe::Section for Elf64Section {
+    fn get_flags(&self) -> u32 {
+        // Always readable
+        let mut flags = 4u32;
+
+        // Writable
+        if self.sh_flags & 1u64 != 0 {
+            flags |= 2;
+        }
+        // Executable
+        if self.sh_flags & 4u64 != 0 {
+            flags |= 1;
+        }
+
+        flags
+    }
+
+    fn get_offset(&self) -> usize {
+        self.sh_offset as usize
+    }
+
+    fn get_size(&self) -> usize {
+        self.sh_size as usize
+    }
+}
+
+impl exe::Exe for Elf64 {
+    type Item = Elf64Section;
+
+    fn get_number_of_sections(&self) -> usize {
+        self.header.e_shnum as usize
+    }
+
+    fn get_section_at(&self, idx: usize) -> Option<&Self::Item> {
+        self.sections.iter().nth(idx)
+    }
 }

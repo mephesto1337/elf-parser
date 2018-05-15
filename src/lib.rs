b04c477f7a885aa4f80097efb02e8af50fc743b2
Thomas WACHE <thomas@wache.fr>
2018-05-15 15:28:14 +0200

    get rid of test mod ans c_api.rs file

--- src/lib.rs
+++ src/lib.rs
@@ -4,9 +4,10 @@ extern crate enum_primitive;
 #[macro_use]
 extern crate nom;
 
-extern crate libc;
-
+#[macro_use]
 extern crate exe;
+
+extern crate libc;
 
 
 #[allow(dead_code)]
@@ -20,14 +21,3 @@ pub use elf32::*;
 
 pub mod elf64;
 pub use elf64::*;
-
-pub mod c_api;
-pub use c_api::*;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
